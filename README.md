949927066dce165d72eefa9685cd9cd0e0f5f5d3
181204kk <jiangsu.pxkk2008@163.com>
2017-09-04 15:40:30 +0800

    p end with the  blockUI 告一段落 还没写完

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ng4CustomerUI
+
+blockUI 使用实例     1.<p-blockUI [blocked]="blockedDocument"></p-blockUI>
+
+                    2.<p-blockUI [target]="pnl" [blocked]="blockedPanel">
+                        <i class="fa fa-lock fa-5x" style="position:absolute;top:25%;left:50%"></i>
+                     </p-blockUI>
--- src/app/components/blockui/blockui.component.ts
+++ src/app/components/blockui/blockui.component.ts
@@ -1,37 +1,61 @@
 import {Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
+import {DomHandler} from "../dom/domhandler";
+import {BlockableUI} from "../common/BlockableUI";
 
 @Component({
   selector: 'app-blockui',
-  template:`<div #mask class="ui-blockui ui-blockui-document" [ngClass]="{'ui-blockui-document': !target}"
+  template:`<div #mask class="ui-blockui ui-widget-overlay" [ngClass]="{'ui-blockui-document': !target}"
     [ngStyle]="{display: blocked?'block':'none'}">
     <ng-content></ng-content>
 </div>
 
 `,
-  styleUrls: ['./blockui.component.css']
+  styleUrls: ['./blockui.component.css'],
+  providers:[DomHandler]
 })
 export class BlockuiComponent implements AfterViewInit,OnInit {
   constructor(private el:ElementRef) { }
+  // 传入的参数 target 如果有target 就不是fixed的遮照
   @Input() target:string;
 
+  // 本组件模板变量
   @ViewChild('mask') mask:ElementRef;
 
+ // 是否显示标志
   _blocked: boolean;
 
+  //
   get blocked():boolean {
     return this._blocked;
   }
 
   @Input() set blocked(val:boolean) {
     this._blocked = val;
-    this._blocked?console.log():console.log();
+    this._blocked?this.block():this.unblock();
   }
 
   ngOnInit() {
 
   }
   ngAfterViewInit(){
+    //实现了getBlockableElement TODO 在修改
+    // if(this.target&&!this.target.getBlockableElement){
+    //   throw 'Target of BlockUI must implement BlockableUI interface';
+    // }
+
     console.log(this.el);
   }
+
+  block():void{
+    console.log(this.target);
+      if(this.target){
+        //TODO target 需要实现getBlockableElement方法 返回target的dom
+        // this.target.getBlockableElement().appendChild(this.mask.nativeElement);
+      }
+  }
+
+  unblock():void{
+    this.el.nativeElement.appendChild(this.mask.nativeElement);
+}
 
 }
