4f33939925177544ccaaa9503504090db296784f
181204kk <jiangsu.pxkk2008@163.com>
2017-09-11 16:43:30 +0800

    panel 第二阶段 引入_theme.css 使用动画  panel content footer一部分

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BlockuiComponent } from './components/blockui/blockui.component';
+import {Panel} from "./components/panel/panel";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlockuiComponent
+    BlockuiComponent,
+    Panel
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/panel/panel.ts
+++ src/app/components/panel/panel.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, Input} from "@angular/core";
 import {BlockableUI} from "../common/blockableui";
+import {trigger,state,style,transition,animate} from '@angular/animations';
 
 @Component({
     selector:'p-panel',
@@ -10,17 +11,38 @@ import {BlockableUI} from "../common/blockableui";
                     <ng-content select="p-header"></ng-content>
                       <a *ngIf="toggleable" href="#" class="ui-panel-titlebar-icon ui-panel-titlebar-toggler 
                       ui-corner-all ui-state-default" (click)="toggle($event)">
-                        <span [class]="collapsed?'fa fa-fw '+expandIcon:'fa fa-fw'+collapseIcon"></span>
+                        <span [class]="collapsed?'fa fa-fw '+expandIcon:'fa fa-fw '+collapseIcon"></span>
                       </a>
                   </div>
-                  <div>
-                    <div></div>
-                    <div></div>
+                  <div class="ui-panel-content-wrapper" [@panelContent]="collapsed?'hidden':'visible'" 
+                    (@panelContent.done)="onToggleDone($event)"
+                    [ngClass]="{'ui-panel-content-wrapper-overflown':collapsed||animating}">
+                    <div class="ui-panel-content ui-widget-content">
+                        <ng-content></ng-content>
+                    </div>
+                    <div class="ui-panel-footer">
+                        <ng-content select="p-footer"></ng-content>
+                    </div>
                   </div>
               </div>
-`
+`,
+  animations:[
+    trigger('panelContent',[
+      state('hidden',style({
+          'height':0
+      })),
+      state('visible',style({
+        height: '*'
+      })),
+      transition('hidden<=>visible',[
+        animate('400ms cubic-bezier(0.86, 0, 0.07, 1)')
+      ])
+    ])
+  ]
 })
 export class Panel implements BlockableUI{
+
+  @Input() toggleable:boolean;
 
   @Input() style:string;
 
@@ -32,11 +54,29 @@ export class Panel implements BlockableUI{
 
   @Input() collapseIcon:string = "fa-minus";
 
+  @Input() collapsed:boolean = false;
+
+  public animating:boolean;
+
   constructor(private el:ElementRef){
 
   }
 
   getBlockableElement():HTMLElement{
     return this.el.nativeElement.children[0];
+  }
+
+  onToggleDone(event:Event){
+    this.animating = false;
+    console.log("panel-onToggleDone");
+  }
+
+  toggle(event:Event){
+    if(this.animating===true){
+      return;
+    }
+    this.animating = true;
+    this.collapsed = !this.collapsed;
+    event.preventDefault();
   }
 }
