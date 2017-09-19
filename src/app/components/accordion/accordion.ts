34d4e65cae50ea8f3ab16fd94b24c278d7b4c0fe
181204kk <jiangsu.pxkk2008@163.com>
2017-09-19 17:24:10 +0800

    1.accordion 第一部分

---
+++ src/app/components/accordion/accordion.ts
@@ -0,0 +1,19 @@
+import {AfterViewInit, OnDestroy, Component} from "@angular/core";
+
+@Component({
+  selector:'accordion',
+  template:`
+    <div class="ui-accordion ui-widget ">
+    
+</div>
+`
+})
+export class Accordion implements AfterViewInit,OnDestroy{
+
+  ngOnDestroy(): void {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+}
