9be74e63491a09e1207fe7972e7ca1dacd8306c5
181204kk <jiangsu.pxkk2008@163.com>
2017-09-20 17:16:51 +0800

    1.accordion 第二部分

--- src/app/components/accordion/accordion.ts
+++ src/app/components/accordion/accordion.ts
@@ -1,19 +1,36 @@
 import {AfterViewInit, OnDestroy, Component} from "@angular/core";
+import {BlockableUI} from "../common/blockableui";
 
 @Component({
-  selector:'accordion',
+  selector:'p-accordion',
   template:`
-    <div class="ui-accordion ui-widget ">
-    
+    <div class="ui-accordion ui-widget ui-helper-reset">
+    <ng-content></ng-content>
 </div>
 `
 })
-export class Accordion implements AfterViewInit,OnDestroy{
+export class Accordion implements BlockableUI{
+
+
+  getBlockableElement(): HTMLElement {
+    return null;
+  }
+}
+
+
+@Component({
+ selector:'p-accordionTab',
+  template:`
+    <div class="ui-accordion-header ui-state-default ui-corner-all">
+    
+    </div>
+    <div >
+    
+    </div>
+  `
+})
+export class AccordionTab implements OnDestroy{
 
   ngOnDestroy(): void {
   }
-
-  ngAfterViewInit(): void {
-  }
-
 }
