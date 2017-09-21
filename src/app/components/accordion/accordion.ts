3be3fc5c796d698215cda2923085fe0439e1978a
181204kk <jiangsu.pxkk2008@163.com>
2017-09-21 17:21:14 +0800

    1.accordion 第三部分

--- src/app/components/accordion/accordion.ts
+++ src/app/components/accordion/accordion.ts
@@ -1,5 +1,6 @@
-import {AfterViewInit, OnDestroy, Component} from "@angular/core";
+import {AfterViewInit, OnDestroy, Component, Input} from "@angular/core";
 import {BlockableUI} from "../common/blockableui";
+import {trigger, state, style, animate, transition} from "@angular/animations";
 
 @Component({
   selector:'p-accordion',
@@ -21,15 +22,41 @@ export class Accordion implements BlockableUI{
 @Component({
  selector:'p-accordionTab',
   template:`
-    <div class="ui-accordion-header ui-state-default ui-corner-all">
-    
+    <div class="ui-accordion-header ui-state-default ui-corner-all" 
+    [ngClass]="{'ui-state-active':selected, 'ui-state-disabled':disabled}"
+    (click)="toggle($event)">
+        <span class="fa fa-fw" [ngClass]="{'fa-caret-down':selected,'fa-caret-right': !selected}"></span>
+        <a href="#" *ngIf="!hasHeaderFacet" role="tab" [attr.aria-expanded]="selected" [attr.aria-selected]="selected">{{header}}</a>
+        <a href="#" *ngIf="hasHeaderFacet" role="tab" [attr.aria-expanded]="selected" [attr.aria-selected]="selected">
+        <ng-content select="p-header"></ng-content></a>
     </div>
-    <div >
-    
+    <div class="ui-accordion-content-wrapper" [@tabContent]="selected?'visible':'hidden'" (@tabContent.done) =onToggleDone($event)
+        [ngClass]="{'ui-accordion-content-wrapper-overflown':!selected||animating}" role="tabpanel" [attr.aria-hidden]="!selected">
+      <div class="ui-accordion-content ui-widget-content" >
+        <ng-content></ng-content>
+</div>
     </div>
-  `
+  `,
+  animations:[
+    trigger('tabContent',[
+        state('hidden',style({
+          height:0
+        })),
+        state('visible',style({
+          height:'*'
+        })),
+      transition('hidden<=>visible', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
+      ]
+    )
+  ]
 })
 export class AccordionTab implements OnDestroy{
+
+  @Input() selected:boolean;
+
+  @Input() disabled:boolean;
+  @Input() hasHeaderFacet:boolean;
+  @Input() animating:boolean;
 
   ngOnDestroy(): void {
   }
