b0c35a07c4d76573efc4fbb005523d6b009cb940
181204kk <jiangsu.pxkk2008@163.com>
2017-10-16 17:12:29 +0800

    splitbutton 第二部分

--- src/app/components/common/menuitem.ts
+++ src/app/components/common/menuitem.ts
@@ -1,4 +1,23 @@
 export interface MenuIterm{
-    label?:string;
-    icon?:string;
+    label?:string;//菜单名称
+    icon?:string;//icon
+
+    expanded?:boolean;//是否展开
+    disabled?:boolean;//是否可用
+    visible?:boolean;//是否可见
+
+    style?:string;//样式
+    classStyle?:string;//类class
+
+    url?:string//页面跳转链接www.
+    routerLink?:string;//页面内跳转 /theme
+    routerLinkActiveOptions?:any; //routerLink选中时传参
+
+  badge?:string;
+  badgeStyleClass?:any;
+
+  iterms?:MenuIterm[];
+
+  command?:any;
+  target?:string;
 }
--- src/app/components/splitbutton/splitbutton.ts
+++ src/app/components/splitbutton/splitbutton.ts
@@ -1,4 +1,5 @@
-import {Component, AfterViewInit, OnDestroy, AfterViewChecked} from "@angular/core";
+import {Component, AfterViewInit, OnDestroy, AfterViewChecked, Input} from "@angular/core";
+import {MenuIterm} from "../common/menuitem";
 @Component({
   selector:'p-splitButton',
   template:`
@@ -11,7 +12,18 @@ import {Component, AfterViewInit, OnDestroy, AfterViewChecked} from "@angular/co
             [class]="menuStyleClass" [style]="menuStyle" [style.display]="menuVisible?'block':'none'">
             <ul class="ui-menu-list ui-helper-reset">
                 <li class="ui-menuitem ui-widget ui-corner-all" role="menuitem"   *ngFor = "let item of model">
-                  
+                  <a *ngIf="!item.routerLink" [href]="item.url||'#'" [attr.target]="item.target" 
+                     [ngClass]="{'ui-state-disabled':item.disabled}" class="ui-menuitem-link ui-corner-all"
+                     (click)="itemClick($event, item)">
+                           <span [ngClass]="'ui-menuitem-icon fa fa-fw'" [class]="item.icon" *ngIf="item.icon"></span>
+                            <span class="ui-menuitem-text">{{item.label}}</span>
+                  </a>
+                  <a *ngIf="item.routerLink" [routerLink] = "item.routerLink" [attr.target]="item.target" 
+                     [ngClass]="{'ui-state-disabled':item.disabled}" class="ui-menuitem-link ui-corner-all"
+                     (click)="itemClick($event, item)">
+                           <span [ngClass]="'ui-menuitem-icon fa fa-fw'" [class]="item.icon" *ngIf="item.icon"></span>
+                          <span class="ui-menuitem-text">{{item.label}}</span>
+                  </a>
                 </li>
             </ul>
         </div>
@@ -21,10 +33,36 @@ import {Component, AfterViewInit, OnDestroy, AfterViewChecked} from "@angular/co
 })
 export class SplitButton implements AfterViewInit,AfterViewChecked,OnDestroy{
 
-  ngOnDestroy(): void {
+  @Input() model:MenuIterm[];
+  @Input() disabled:boolean;
+  @Input() style:string;
+  @Input() styleClass:string;
+  @Input() menuStyle:string;
+  @Input() menuVisible:boolean;
+  @Input() menuStyleClass:string;
+  @Input() icon:string;
+  @Input() iconPos:string;
+  @Input() label:string;
+  @Input() tabindex:number;
+
+
+
+
+
+  onDefaultButtonClick():void{
+
+  }
+  onDropdownButtonClick():void{
+
+  }
+  itemClick():void{
+
   }
 
   ngAfterViewInit(): void {
+  }
+
+  ngOnDestroy(): void {
   }
 
 
